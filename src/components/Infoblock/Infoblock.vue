8d42c614f96e5596985d7c4dd7cc0b536e822836
vik <vik@gmail.com>
2023-06-20 20:55:37 +0500

    infoblock

---
+++ src/components/Infoblock/Infoblock.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main__info">
+        <img src="@/assets/images/close.svg" class="main__info-close" @click="$emit('close')" alt="">
+        <div class="main__info-block">
+            <img src="@/assets/images/block.jpg" class="main__info-img" alt="">
+            <div class="main__info-contetn">
+                <div class="main__info-contetn-block">
+                    <h2 class="main__info-contetn-title">Ишан-чи</h2>
+                    <p class="main__info-contetn-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum ducimus nam sit exercitationem a rerum delectus quam odit dignissimos iste.
+                    </p>
+                    <p class="main__info-contetn-date">
+                        <span>2023</span>
+                        <span >жынры</span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { imgFull } from '@/static'
+const props = defineProps(['current', 'type'])
+
+</script>
--- src/components/MOT/Content.vue
+++ src/components/MOT/Content.vue
@@ -5,16 +5,19 @@
             <img src="@/assets/images/arrow.png" alt="">
         </router-link>
         <Swiper :modules="modules" :space-between="25" :navigation="true" :breakpoints="swiperOptions.breakpoints">
-            <Swiper-slide 
-                class="main__video-item"
-                v-for="(item, idx) in content"
-                :key="item.id"
-            >
-                <img v-lazy="imgUrlFull+item.poster_path" alt="" class="main__video-item-img">
-                <router-link :to="`${props.type}/`" />
+            <Swiper-slide class="main__video-item" v-for="(item, idx) in content" :key="item.id">
+                <img v-lazy="imgUrlFull + item.poster_path" alt="" class="main__video-item-img">
+                <router-link :to="`${props.type}/`"  class="main__video-item-link"/>
                 <h2 class="main__video-item-title">{{ item.title || item.name }}</h2>
             </Swiper-slide>
+            <Swiper-slide >
+                <router-link :to="`${props.type}/`" class="main__video-item">
+                    <span>{{ props.type == 'movie' ? 'Все Фильмы' : 'Все Сериалы' }}</span>
+                </router-link>
+            </Swiper-slide>
+
         </Swiper>
+        <InfoBlock :current="current" :type="type" @close="close"/>
     </section>
 </template>
 
@@ -26,11 +29,12 @@ import 'swiper/scss/navigation'
 import { usePopular } from '@/stores/popular'
 import { onMounted, ref, computed } from 'vue';
 import { imgUrl, imgUrlFull } from "@/static.js";
+import InfoBlock from '@/components/Infoblock/Infoblock.vue'
 
 const props = defineProps(['type'])
 const popular = usePopular()
 
-let modules = ref(Navigation)
+let modules = ref([Navigation])
 let swiperOptions = ref({
     breakpoints: {
         320: {
@@ -57,6 +61,12 @@ onMounted(() => {
     popular.getPopular({ type: props.type })
 })
 
+let current = ref(null)
+let inf = ref(null)
+
+const close = () =>{
+    console.log(current.value);
+}
 </script>
 
 <style lang="scss" scoped></style>
